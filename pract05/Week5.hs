76eb495626802a6dc5273f3f0c854c882bfe2054
root <zemorais16@hotmail.com>
2019-03-07 02:01:56 +0000

    End of the worksheet

--- pract05/Week5.hs
+++ pract05/Week5.hs
@@ -38,8 +38,11 @@ zip :: [a] -> [b] -> [(a,b)]
 zip (x:xs) (y:ys)  = (x,y) : zip xs ys
 zip _ _            = []
 
+type StudentMark = (String, Int)
+
 -- worksheet
 
+-- list patterns
 headPlusOne :: [Int] -> Int
 headPlusOne [] = 0
 headPlusOne (x:xs) = head (x:xs) + 1
@@ -48,5 +51,45 @@ duplicateHead :: [a] -> [a]
 duplicateHead []  = []
 duplicateHead (x:xs) = head (x:xs) : (x:xs)
 
--- rotate :: [a] -> [a]
--- rotate (x:xs) = head (tail (x:xs)) : head (x:xs) : tail (head (tail (x:xs)))
+rotate :: [a] -> [a]
+rotate (x:xs) = if length (x:xs) < 2 then (x:xs) else head xs : x : tail xs ++ []
+
+-- recursion over lists
+listLength :: [a] -> Int
+listLength [] = 0
+listLength (x:xs) = 1 + listLength xs
+
+multAll :: [Int] -> Int
+multAll [] = 1
+multAll (x:xs) = x * multAll xs
+
+andAll :: [Bool] -> Bool
+andAll [] = True
+andAll (x:xs) = x && andAll xs
+
+countElems :: Int -> [Int] -> Int
+countElems _ [] = 0
+countElems i (x:xs)
+  | i == x = 1 + countElems i xs
+  | otherwise = countElems i xs
+
+removeAll :: Int -> [Int] -> [Int]
+removeAll _ [] = []
+removeAll i (x:xs)
+  | i == x = removeAll x xs
+  | otherwise = x : removeAll i xs
+
+listMarks :: String -> [StudentMark] -> [Int]
+listMarks _ [] = []
+listMarks std (x:xs)
+  | std == fst x = snd x : listMarks std xs
+  | otherwise = listMarks std xs
+
+prefix :: [Int] -> [Int] -> Bool
+prefix [] _ = True
+prefix (x:xs) (y:ys)
+  | x /= y = False
+  | otherwise = prefix xs ys
+
+subSequence :: [Int] -> [Int] -> Bool
+subSequence xs (y:ys) = prefix xs (y:ys) || subSequence xs ys
