2ea07bfc069b32829eae72adb8539a3e565d4846
root <zemorais16@hotmail.com>
2019-04-22 19:57:22 +0100

    End of the worksheet

--- pract06/Week6.hs
+++ pract06/Week6.hs
@@ -36,6 +36,7 @@ mult10 = map (*10)
 
 onlyLowerCase = filter isLower
 
+-- this function finds the disjunction (or) of the elements in a Boolean list
 orAll bs = foldr (||) False bs
 
 sumSquares = addUp . map(^2)
@@ -45,3 +46,21 @@ zeroToTen = filter (>=0) . filter (<=10)
 squareRoots = map (sqrt) . filter (>0)
 
 countBetween x y = length . filter(>=x) . filter(<=y)
+
+alwaysPositive f (x:xs) = if (foldr (+) 0 (map f (x:xs))) > 0 then True else False
+
+productSquareRoots (x:xs) = foldr (*) 1 (filter (>0) (map sqrt (x:xs)))
+
+removeFirst :: (a -> Bool) -> [a] -> [a]
+removeFirst _ [] = []
+removeFirst p (x:xs)
+    | not (p x) = x : removeFirst p xs
+    | otherwise = xs
+
+removeLast :: (a -> Bool) -> [a] -> [a]
+removeLast _ [] = []
+removeLast p xs
+    | not (p (last xs)) = removeLast p (init xs) ++ [last xs]
+    | otherwise = init xs
+
+zeroToTenLambda = filter (\x -> x>=0 && x<=10)
